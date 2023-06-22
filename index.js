64a3eecf155f985367068c515e0fba40cba5bff4
danel-torre-ubuntu <dlafuente003@gmail.com>
2023-06-22 22:15:53 +0200

    estilo y comentarios

--- index.js
+++ index.js
@@ -1,48 +1,42 @@
 import Painter from "./paint.js";
 
-const canvas = document.getElementById("canvas");
-let painter = new Painter(canvas);
-let colorPicker = document.createElement("input");
-colorPicker.type = "color";
-colorPicker.addEventListener("change", (e) => {
-    painter.changeColor(e.target.value);
-});
-document.body.appendChild(colorPicker);
-let radiusPicker = document.createElement("input");
-radiusPicker.type = "range";
-radiusPicker.min = 1;
-radiusPicker.max = 100;
-radiusPicker.value = 5;
-radiusPicker.addEventListener("change", (e) => {
-    painter.changeRadius(e.target.value);
-});
-document.body.appendChild(radiusPicker);
+const canvas = document.getElementById("canvas"); // se obtiene el canvas
+let painter = new Painter(canvas); // se crea un objeto Painter
 
-let downloadButton = document.createElement("button");
-downloadButton.innerText = "Download";
-downloadButton.addEventListener("click", () => {
-    painter.saveCanvas();
+painter.changeColor(document.getElementById("color-picker").value); // se cambia el color del lápiz
+painter.changeRadius(document.getElementById("brush-size").value); // se cambia el radio del lápiz
+
+let colorPicker = document.getElementById("color-picker"); // se obtiene el selector de color
+colorPicker.addEventListener("change", (e) => { // cuando cambia el color
+    painter.changeColor(e.target.value); // se cambia el color del lápiz
+});
+
+let radiusPicker = document.getElementById("brush-size"); // se obtiene el selector de radio
+radiusPicker.addEventListener("change", (e) => { // cuando cambia el radio
+    painter.changeRadius(e.target.value); // se cambia el radio del lápiz
+});
+
+let downloadButton = document.getElementById("download"); // se obtiene el botón de descarga
+downloadButton.addEventListener("click", () => { // cuando se hace click en el botón de descarga
+    painter.saveCanvas(); // se guarda el canvas
 }
 );
-document.body.appendChild(downloadButton);
 
-let clearButton = document.createElement("button");
-clearButton.innerText = "Clear";
-clearButton.addEventListener("click", () => {
-    painter.clearCanvas();
+let clearButton = document.getElementById("clear"); // se obtiene el botón de limpiar
+clearButton.addEventListener("click", () => { // cuando se hace click en el botón de limpiar
+    painter.clearCanvas(); // se limpia el canvas
 }
 );
-document.body.appendChild(clearButton);
 
-let eraseButton = document.createElement("button");
-eraseButton.innerText = "Erase";
-eraseButton.addEventListener("click", () => {
-    if (painter.mode === "erase") {
-        painter.changeMode("paint");
-        eraseButton.innerText = "Erase";
+let eraseButton = document.getElementById("mode"); // se obtiene el botón de borrar
+eraseButton.addEventListener("click", () => { // cuando se hace click en el botón de borrar
+    if (painter.mode === "erase") { // si el modo es borrar
+        painter.changeMode("paint"); // se cambia el modo a pintar
+        eraseButton.innerText = "Erase"; // se cambia el texto del botón
+        document.getElementById("canvas").classList.remove("eraser"); // se quita la clase eraser
         return;
     }
-    painter.changeMode("erase");
-    eraseButton.innerText = "Paint";
+    painter.changeMode("erase"); // se cambia el modo a borrar
+    eraseButton.innerText = "Paint"; // se cambia el texto del botón
+    document.getElementById("canvas").classList.add("eraser"); // se agrega la clase eraser
 });
-document.body.appendChild(eraseButton);
--- paint.js
+++ paint.js
@@ -4,85 +4,87 @@
 
 class Painter {
     constructor(canvas) {
-        this.paintColor = "black";
-        this.paintRadius = 5;
-        this.eraseRadius = 10;
-        this.canvas = canvas;
-        this.ctx = canvas.getContext("2d");
-        this.ctx.lineWidth = this.paintRadius * 2;
-        this.ctx.lineCap = "round";
-        this.ctx.strokeStyle = this.paintColor;
-        this.ctx.fillStyle = this.paintColor;
-        this.ctx.globalCompositeOperation = "source-over";
-        this.isPainting = false;
-        this.isErasing = false;
-        this.lastX = 0;
-        this.lastY = 0;
-        this.lastCommands = [];
-        this.canvas.addEventListener("mousedown", this.startPainting.bind(this));
-        this.canvas.addEventListener("mousemove", this.paint.bind(this));
-        this.canvas.addEventListener("mouseup", this.stopPainting.bind(this));
-        this.canvas.addEventListener("mouseout", this.stopPainting.bind(this));
-        this.canvas.addEventListener("touchstart", this.startPainting.bind(this));
-        this.canvas.addEventListener("touchmove", this.paint.bind(this));
-        this.canvas.addEventListener("touchend", this.stopPainting.bind(this));
-        this.canvas.addEventListener("touchcancel", this.stopPainting.bind(this));
+        this.paintColor = "black"; // color del lápiz
+        this.paintRadius = 5; // radio del lápiz
+        this.canvas = canvas; // canvas
+        this.ctx = canvas.getContext("2d"); // contexto del canvas
+        this.ctx.lineWidth = this.paintRadius * 2; // ancho de la línea
+        this.ctx.lineCap = "round"; // tipo de línea
+        this.ctx.strokeStyle = this.paintColor; // color de la línea
+        this.ctx.fillStyle = this.paintColor; // color de relleno
+        this.ctx.globalCompositeOperation = "source-over"; // modo de dibujado. source-over es el modo por defecto, que dibuja sobre el canvas
+        this.isPainting = false; // indica si se está pintando o no
+        this.lastX = 0; // última posición x
+        this.lastY = 0; // última posición y
+        this.addListeners();
     }
-    startPainting(e) {
+    addListeners() {
+        this.canvas.addEventListener("mousedown", (e) =>this.startPainting(e)); // cuando se pulsa el ratón, se empieza a pintar
+        this.canvas.addEventListener("mousemove", (e) => this.paint(e)); // cuando se mueve el ratón, se pinta
+        this.canvas.addEventListener("mouseup", (e) => this.stopPainting(e)); // cuando se deja de pulsar el ratón, se deja de pintar
+        this.canvas.addEventListener("mouseout", (e) =>this.stopPainting(e)); // cuando el ratón sale del canvas, se deja de pintar
+        this.canvas.addEventListener("touchstart", (e) => this.startPainting(e,true)); // cuando se toca la pantalla, se empieza a pintar
+        this.canvas.addEventListener("touchmove", (e) =>this.paint(e,true)); // cuando se mueve el dedo por la pantalla, se pinta
+        this.canvas.addEventListener("touchend", (e) => this.stopPainting(e)); // cuando se deja de tocar la pantalla, se deja de pintar
+        this.canvas.addEventListener("touchcancel", (e) =>this.stopPainting(e)); // cuando se cancela el evento touch, se deja de pintar
+    }
+    startPainting(e,isTouch=false) {
         this.isPainting = true;
-        this.isErasing = false;
-        this.lastX = e.offsetX;
-        this.lastY = e.offsetY;
+        if (isTouch) { // si es un evento touch, se guarda la posición del dedo
+            this.lastX = e.touches[0].clientX - this.canvas.offsetLeft; // posición x del dedo
+            this.lastY = e.touches[0].clientY - this.canvas.offsetTop; // posición y del dedo
+            return;
+        }
+        this.lastX = e.offsetX; // posición x del ratón
+        this.lastY = e.offsetY; // posición y del ratón
     }
     stopPainting() {
         this.isPainting = false;
-        this.isErasing = false;
     }
-    paint(e) {
+    paint(e,isTouch=false) {
         if (this.isPainting) {
-            this.ctx.beginPath();
-            this.ctx.moveTo(this.lastX, this.lastY);
-            this.ctx.lineTo(e.offsetX, e.offsetY);
-            this.ctx.stroke();
-            this.lastX = e.offsetX;
-            this.lastY = e.offsetY;
+            if (isTouch) { // si es un evento touch, se pinta desde la última posición del dedo
+                this.ctx.beginPath(); // se empieza a dibujar
+                this.ctx.moveTo(this.lastX, this.lastY); // se mueve el lápiz a la última posición del dedo
+                this.ctx.lineTo(e.touches[0].clientX - this.canvas.offsetLeft, e.touches[0].clientY - this.canvas.offsetTop); // se dibuja una línea hasta la posición actual del dedo
+                this.ctx.stroke(); // se dibuja la línea
+                this.lastX = e.touches[0].clientX - this.canvas.offsetLeft; // se guarda la última posición del dedo
+                this.lastY = e.touches[0].clientY - this.canvas.offsetTop; // se guarda la última posición del dedo
+                return;
+            }
+            this.ctx.beginPath(); // se empieza a dibujar
+            this.ctx.moveTo(this.lastX, this.lastY); // se mueve el lápiz a la última posición del ratón
+            this.ctx.lineTo(e.offsetX, e.offsetY); // se dibuja una línea hasta la posición actual del ratón
+            this.ctx.stroke(); // se dibuja la línea
+            this.lastX = e.offsetX; // se guarda la última posición del ratón
+            this.lastY = e.offsetY; // se guarda la última posición del ratón
         }
-    }
-    erase(e) {
-        if (this.isErasing) {
-            this.ctx.beginPath();
-            this.ctx.arc(e.offsetX, e.offsetY, this.eraseRadius, 0, Math.PI * 2);
-            this.ctx.fill();
-        }
-    }
+    } 
     changeColor(color) {
-        this.paintColor = color;
-        this.ctx.strokeStyle = this.paintColor;
-        this.ctx.fillStyle = this.paintColor;
+        this.paintColor = color; // se cambia el color del lápiz
+        this.ctx.strokeStyle = this.paintColor; // se cambia el color de la línea
+        this.ctx.fillStyle = this.paintColor; // se cambia el color de relleno
     }
     changeRadius(radius) {
-        this.paintRadius = radius;
-        this.ctx.lineWidth = this.paintRadius * 2;
-    }
-    changeEraseRadius(radius) {
-        this.eraseRadius = radius;
+        this.paintRadius = radius; // se cambia el radio del lápiz
+        this.ctx.lineWidth = this.paintRadius * 2; // se cambia el ancho de la línea
     }
     clearCanvas() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // se limpia el canvas
     }
     saveCanvas() {
-        let link = document.createElement("a");
-        link.download = "canvas.png";
-        link.href = this.canvas.toDataURL();
-        link.click();
+        let link = document.createElement("a"); // se crea un enlace
+        link.download = "canvas.png"; // se le da un nombre al archivo
+        link.href = this.canvas.toDataURL(); // se le asigna el contenido del canvas
+        link.click(); // se hace click en el enlace
     }
     changeMode(mode) {
-        if (mode === "paint") {
-            this.mode = "paint";
-            this.ctx.globalCompositeOperation = "source-over";
+        if (mode === "paint") {  
+            this.mode = "paint"; // se cambia el modo a pintar
+            this.ctx.globalCompositeOperation = "source-over"; // se cambia el modo de dibujado a source-over, que dibuja sobre el canvas
         } else if (mode === "erase") {
-            this.mode = "erase";
-            this.ctx.globalCompositeOperation = "destination-out";
+            this.mode = "erase"; // se cambia el modo a borrar
+            this.ctx.globalCompositeOperation = "destination-out"; // se cambia el modo de dibujado a destination-out, que borra lo que se dibuja sobre el canvas
         }
     }
 }
